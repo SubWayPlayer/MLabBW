c837331dba0f819499898caa2c590fa4a5a5c7b3
Sub_Way_Player <Alexander@DESKTOP-AMRGD2Q.fritz.box>
2017-04-09 18:33:43 +0200

    BedWars Projekt

---
+++ BedWars/src/net/mysterylab/bedwars/main/BedWars.java
@@ -0,0 +1,14 @@
+package net.mysterylab.bedwars.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BedWars extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		
+		
+		
+	}
+	
+}
